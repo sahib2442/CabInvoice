b7ee7741c1918c326f7fb46089c4f19bb6b16304
sahib2442 <kadersahib1234@gmail.com>
2022-10-25 10:35:07 +0530

    [sahib] Add: MapRide

--- CabInvoiceGenerator/CabInvoiceGenerator.cs
+++ CabInvoiceGenerator/CabInvoiceGenerator.cs
@@ -13,6 +13,7 @@ namespace CabInvoiceGenerator
         private static readonly double COST_PER_MINUTE = 1.0;
         private static readonly double MINIMUM_FARE = 5.0;
         private double cabFare = 0.0;
+        private RideRepository rideRepository = new RideRepository();
         public double CalculateFare(double distance, double time)
         {
             this.cabFare = (distance * COST_PER_KILOMETER) + (time * COST_PER_MINUTE);
@@ -32,6 +33,16 @@ namespace CabInvoiceGenerator
             InvoiceSummary data = GetMultipleRideFare(ride);
             Console.WriteLine(data.averageFare + "\n" + data.noOfRides);
         }
+        public void MapRidesToUser(string userID, Ride[] rides)
+        {
+            this.rideRepository.AddCabRides(userID, rides);
+        }
+        public InvoiceSummary GetInvoiceSummary(string userID)
+        {
+            return this.GetMultipleRideFare(this.rideRepository.GetCabRides(userID));
+        }
+    }
+}
     }
 }
 
