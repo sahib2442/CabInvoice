e94d828da12a7876ddaa53ebeb24a231cbc1b63f
sahib2442 <kadersahib1234@gmail.com>
2022-10-25 10:31:10 +0530

    [sahib] Add: Ride Repositary

---
+++ CabInvoiceGenerator/RideRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    public class RideRepository
+    {
+        private Dictionary<string, List<Ride>> userRideList = new Dictionary<string, List<Ride>>();
+        public void AddCabRides(string userID, Ride[] ride)
+        {
+            bool checkList = this.userRideList.ContainsKey(userID);
+            if (checkList == false)
+            {
+                this.userRideList.Add(userID, new List<Ride>(ride));
+            }
+        }
+        public Ride[] GetCabRides(string userId)
+        {
+            return this.userRideList[userId].ToArray();
+        }
+    }
+}
